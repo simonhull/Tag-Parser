24eccd917e53459a6e3dd5f56f8f1d578ac0a6f3
simonhull <simonthomashull@gmail.com>
2024-08-05 11:00:30 -0600

    Updated Module

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/simonhull/Tag-Parser
 
 go 1.20
-
-require github.com/unitnotes/audiotag v0.1.0
--- main.go
+++ main.go
@@ -3,15 +3,28 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/simonhull/Tag-Parser/audiotag"
+	"github.com/simonhull/Tag-Parser/go-mp4tag"
+	"log"
 	"os"
 	"path/filepath"
 	"strconv"
-
-	"main/go-mp4tag"
-
-	"main/audiotag"
 )
 
+func parseAudioTag(filePath string) (audiotag.Metadata, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		fmt.Println("Parse Error:", err.Error())
+	}
+	m, err := audiotag.ReadFrom(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Print(m.Format()) // The detected format.
+	log.Print(m.Title())  // The title of the track (see Metadata interface for more details).
+
+	return m, nil
+}
 func main() {
 
 	filePath := flag.String("file", "", "path to the file")
